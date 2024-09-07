934bf9058ccfcf7991e4cbf5649fc39eb2f75f78
empzhang22 <epz6@cornell.edu>
2024-09-07 13:43:18 -0400

    Created folder, added skeleton files for testing

---
+++ image_flattening/homography.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy
+
+# the list of the "real" corners from the raw image
+# in order:
+#   corners[0]: top left
+#   corners[1]: top right
+#   corners[2]: bottom right
+#   corners[3]: bottom left
+corners = []
