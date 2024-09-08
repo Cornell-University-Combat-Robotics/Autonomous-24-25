4553bc89ed5d2a092a7c7799364f592a5fc798ad
shaostassen <116751423+ses439@users.noreply.github.com>
2024-09-08 12:16:23 -0400

    Complete video-stream & warp function integration

--- image_flattening/warp.py
+++ image_flattening/warp.py
@@ -15,7 +15,7 @@ def warp(source, WIDTH=600, HEIGHT=600):
        [ 1.07684499e-05,  2.64804311e-03,  1.00000000e+00]]
     return cv2.warpPerspective(source, np.array(matrix), (WIDTH, HEIGHT))
 
-src = cv2.imread('cage_overhead_1.png')
-cv2.imshow("Image", warp(src, 600, 600))
-cv2.waitKey(0)
+# src = cv2.imread('cage_overhead_1.png')
+# cv2.imshow("Image", warp(src, 600, 600))
+# cv2.waitKey(0)
 
