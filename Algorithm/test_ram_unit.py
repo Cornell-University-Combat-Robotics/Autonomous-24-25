506679da9cb4d69b6ce75717e265ad6e15a6abc9
Shao Stassen <75188619+shaostassen@users.noreply.github.com>
2025-01-14 14:42:22 -0500

    added more unit tests

--- Algorithm/test_ram_unit.py
+++ Algorithm/test_ram_unit.py
@@ -15,12 +15,12 @@ class TestRam(unittest.TestCase):
     def test_huey_move(self):
             # edge cases
             move_dict = algo.huey_move(0,-1)
-            self.assertAlmostEqual(move_dict['left'],-0.5)
-            self.assertAlmostEqual(move_dict['right'],0.5)
-
-            move_dict = algo.huey_move(0,1)
             self.assertAlmostEqual(move_dict['left'],0.5)
             self.assertAlmostEqual(move_dict['right'],-0.5)
+
+            move_dict = algo.huey_move(0,1)
+            self.assertAlmostEqual(move_dict['left'],-0.5)
+            self.assertAlmostEqual(move_dict['right'],0.5)
 
             move_dict = algo.huey_move(1,0)
             self.assertAlmostEqual(move_dict['left'], 0.5)
@@ -32,12 +32,12 @@ class TestRam(unittest.TestCase):
             
             # intermediate values
             move_dict = algo.huey_move(0.3,-0.5)
-            self.assertAlmostEqual(move_dict['left'], -0.1)
-            self.assertAlmostEqual(move_dict['right'], 0.4)
+            self.assertAlmostEqual(move_dict['left'], 0.4)
+            self.assertAlmostEqual(move_dict['right'], -0.1)
 
             move_dict = algo.huey_move(0.7,0.2)
-            self.assertAlmostEqual(move_dict['left'], 0.45)
-            self.assertAlmostEqual(move_dict['right'], 0.25)
+            self.assertAlmostEqual(move_dict['left'], 0.25)
+            self.assertAlmostEqual(move_dict['right'], 0.45)
 
 
     def test_calculate_velocity(self):
@@ -180,7 +180,11 @@ class TestRam(unittest.TestCase):
         
         # Funky case
         np.testing.assert_almost_equal(algo.predict_desired_orientation_angle(our_pos=np.array([100,100]), our_orientation=100,
-                                                                                enemy_pos=np.array([150,150]), enemy_velocity=np.array([0, 0]), dt = 0.1), -10)
+                                                                                enemy_pos=np.array([150,150]), enemy_velocity=np.array([200, 300]), dt = 0.1), -148.81407483)
+        
+        # orientation > 180
+        np.testing.assert_almost_equal(algo.predict_desired_orientation_angle(our_pos=np.array([100,100]), our_orientation=270,
+                                                                                enemy_pos=np.array([150,150]), enemy_velocity=np.array([200, 300]), dt = 0.1), 41.1859252)
         
 
     def test_predict_desired_turn(self):
@@ -221,4 +225,61 @@ class TestRam(unittest.TestCase):
         np.testing.assert_almost_equal(algo.predict_desired_turn(our_pos=np.array([100,100]), our_orientation=90,  
                                                                                 enemy_pos=np.array([100,100]), enemy_velocity=np.array([0, 0]), dt = 0.1), 0)
         
-    # def test_predict_desired_turn():
+    def test_desired_speed(self):
+        #Edge Cases
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([100,0]), enemy_velocity=np.array([0, 0]), dt = 0.1), 1)
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([0, 0]), dt = 0.1), 0.5)
+         # 180 degrees
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([100,600]), enemy_velocity=np.array([0, 0]), dt = 0.1), 0)
+        # 90 degrees
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([0,100]), enemy_velocity=np.array([0, 0]), dt = 0.1), 0.5)
+
+        # -38 degrees
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([150,36.002981839]), enemy_velocity=np.array([0, 0]), dt = 0.1), 0.788888889, decimal=2)
+        #  dt = 0
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([100, 100]), dt = 0), 0.5)
+        # velocity = west
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=0,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([-100, 0]), dt = 0.1), 1)
+        # velocity = east
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=0,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([100, 0]), dt = 0.1), 1)
+        # velocity = south
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=0,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([0, 100]), dt = 0.1), 0.96827448258)
+
+        # velocity = north
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=0,  
+                                                                              enemy_pos=np.array([200,100]), enemy_velocity=np.array([0, -100]), dt = 0.1),0.96827448258)
+        
+        # bots are overlapping
+        np.testing.assert_almost_equal(algo.predict_desired_speed(our_pos=np.array([100,100]), our_orientation=90,  
+                                                                                enemy_pos=np.array([100,100]), enemy_velocity=np.array([0, 0]), dt = 0.1), 1)
+    def test_ram_ram(self):
+        algo = Ram(huey_old_position=np.array([10,10]), huey_position=np.array([10,10]), enemy_position=np.array([10,590]))
+        bots1 = {'huey': {'bb': [0, 0, 20, 20], 'center': [10, 10], 'orientation': 0.0}, 'enemy': {'bb': [780, 580, 20, 20], 'center': [10, 590]}}
+        values = algo.ram_ram(bots1)
+        self.assertAlmostEqual(values['left'], .5) 
+        self.assertAlmostEqual(values['right'], 0) 
+        bots2 = {'huey': {'bb': [138, 154, 20, 20], 'center': [148, 164], 'orientation': 265.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+        values = algo.ram_ram(bots2)
+        self.assertAlmostEqual(values['left'], -0.0766318353)
+        self.assertAlmostEqual(values['right'], 0.5)
+
+
+
+bots3 = {'huey': {'bb': [199.21307092309192, 397.12606428385743, 20, 20], 'center': [209.21307092309192, 407.12606428385743], 'orientation': 270.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots4 = {'huey': {'bb': [99.13626817533684, 582.1403853383393, 20, 20], 'center': [109.13626817533684, 592.1403853383393], 'orientation': 325.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots5 = {'huey': {'bb': [175.86020593421904, 400.73527537699533, 20, 20], 'center': [185.86020593421904, 410.73527537699533], 'orientation': 95.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots6 = {'huey': {'bb': [210.82217004773602, 172.0585415440591, 20, 20], 'center': [220.82217004773602, 182.0585415440591], 'orientation': 10.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots7 = {'huey': {'bb': [401.9807865634781, 331.582780056669, 20, 20], 'center': [411.9807865634781, 341.582780056669], 'orientation': 315.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots8 = {'huey': {'bb': [530.5537075054144, 226.91770778196855, 20, 20], 'center': [540.5537075054144, 236.91770778196855], 'orientation': 85.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots9 = {'huey': {'bb': [421.700003041856, 157.16505240600515, 20, 20], 'center': [431.700003041856, 167.16505240600515], 'orientation': 265.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots10 = {'huey': {'bb': [528.4292586105291, 295.3309900724159, 20, 20], 'center': [538.4292586105291, 305.3309900724159], 'orientation': 25.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
+bots11 = {'huey': {'bb': [485.45775720605434, 173.0092132559673, 20, 20], 'center': [495.45775720605434, 183.0092132559673], 'orientation': 185.0}, 'enemy': {'bb': [500, 210, 20, 20], 'center': [510, 220]}}
