e156f9f808981617b3e6330d57d3b84014404790
rohin20 <rohinphukan@gmail.com>
2025-01-16 14:42:17 -0500

    roboflow v9

--- machine/test_models.py
+++ machine/test_models.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 import time
-from predict import YoloModel
+from predict import YoloModel 
+from predict import RoboflowModel
 
-def test_model(model_name,model_type, device = None, img = '12567_png.rf.6bb2ea773419cd7ef9c75502af6fe808.jpg'):
-    print('starting testing with PT model')
-    predictor = YoloModel(model_name, model_type, device)
+def test_model(method,model_name,model_type, device = None, img = '12567_png.rf.6bb2ea773419cd7ef9c75502af6fe808.jpg'):
+    if method == 'YoloModel':
+        print('starting testing with PT model')
+        predictor = YoloModel(model_name, model_type, device)
+    elif method == 'RoboflowModel':
+        print('starting testing with Roboflow model')
+        predictor = RoboflowModel()
 
     img = cv2.imread(img)
 
@@ -14,7 +19,7 @@ def test_model(model_name,model_type, device = None, img = '12567_png.rf.6bb2ea7
     data = np.zeros(100)
     for i in range(100):
         start_time = time.time()
-        bots = predictor.predict(img, show=False)
+        predictor.predict(img, show=False)
         end_time = time.time()
         elapsed = end_time - start_time
         #print(f'elapsed time: {elapsed:.4f}')
@@ -24,14 +29,44 @@ def test_model(model_name,model_type, device = None, img = '12567_png.rf.6bb2ea7
     return data
 
 if __name__ == "__main__":
-    models = ['ONNX', 'PT']
-    with open('test_results.txt', 'w') as file:
-        for model in models:
-            data = test_model('100epoch11',model)
+    method = input("Method: y or r")
+    if method == 'YoloModel' or 'y':
+        models = ['ONNX', 'PT']
+        with open('test_results.txt', 'w') as file:
+            for model in models:
+                data = test_model('100epoch11',model)
+                avg = np.mean(data)
+                avgn1 = np.mean(data[1:])
+                med = np.median(data)
+                file.write(f'{model} = {data}')
+                file.write('\n')
+                file.write(f'Average: {avg}')
+                file.write('\n')
+                file.write(f'Average no first: {avgn1}')
+                file.write('\n')
+                file.write(f'Median: {med}')
+                file.write('\n')
+
+            for model in ['PT']:
+                data = test_model('100epoch11',model, device = "mps")
+                avg = np.mean(data)
+                avgn1 = np.mean(data[1:])
+                med = np.median(data)
+                file.write(f'{model}, mps = {data}')
+                file.write('\n')
+                file.write(f'Average: {avg}')
+                file.write('\n')
+                file.write(f'Average no first: {avgn1}')
+                file.write('\n')
+                file.write(f'Median: {med}')
+                file.write('\n')
+    elif method == 'RoboflowModel' or 'r':
+        with open('test_results.txt', 'w') as file:
+            data = test_model(method = 'RoboflowModel', model_name = None, model_type = None)
             avg = np.mean(data)
             avgn1 = np.mean(data[1:])
             med = np.median(data)
-            file.write(f'{model} = {data}')
+            file.write(f'Roboflow = {data}')
             file.write('\n')
             file.write(f'Average: {avg}')
             file.write('\n')
@@ -40,16 +75,5 @@ if __name__ == "__main__":
             file.write(f'Median: {med}')
             file.write('\n')
 
-        for model in ['PT']:
-            data = test_model('100epoch11',model, device = "mps")
-            avg = np.mean(data)
-            avgn1 = np.mean(data[1:])
-            med = np.median(data)
-            file.write(f'{model}, mps = {data}')
-            file.write('\n')
-            file.write(f'Average: {avg}')
-            file.write('\n')
-            file.write(f'Average no first: {avgn1}')
-            file.write('\n')
-            file.write(f'Median: {med}')
-            file.write('\n')
+    else:
+        print("you suck and messed up")
