fd958b06f173c29d671a7c2e9fe32ab46de7f12f
rohin20 <rohinphukan@gmail.com>
2025-02-22 10:25:25 -0500

    test mode false

--- Algorithm/ram.py
+++ Algorithm/ram.py
@@ -67,7 +67,7 @@ class Ram():
     MIN_TURN = 0 # between 0 and 1
     ARENA_WIDTH = 1200 # in pixels
     BATTLE_MODE = False # this is true when the match actually has begun and will cause the motors to move
-    TEST_MODE = True # saves values to CSV file
+    TEST_MODE = False # saves values to CSV file
 
     '''
     Constructor for the Ram class that initializes the position and orientation of the bot, the motors, the enemy position, 
