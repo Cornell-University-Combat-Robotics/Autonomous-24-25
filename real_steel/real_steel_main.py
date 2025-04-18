e2a29324dbe3e7a8841c065b84d63b8510c08eb1
Aaron Harnish <aaronjharnish@gmail.com>
2025-04-18 12:23:04 -0400

    Last real steel changes

--- real_steel/real_steel_main.py
+++ real_steel/real_steel_main.py
@@ -38,19 +38,20 @@ print(
 The input for throttle or turn will depend on the position of your selected 'mover' relative to your selected 'reference'.
 """)
 
-throt_move = input("Throttle Mover (0-16): ")
-throt_ref = input("Throttle Reference (0-16): ")
-turn_move = input("Turn Mover (0-16): ")
-turn_ref = input("Turn Reference (0-16): ")
+throt_move = int(input("Throttle Mover (0-16): "))
+throt_ref = int(input("Throttle Reference (0-16): "))
+turn_move = int(input("Turn Mover (0-16): "))
+turn_ref = int(input("Turn Reference (0-16): "))
 
 # Create low, high values
 turn_low = np.array([0.18, -0.1])
 turn_high = np.array([0.18, -0.3])
 turn_line = turn_high - turn_low
 
-bounds = (None, None, None, None)
+bounds = [None, None, None, None]
+empty_bounds = True
 
-while cap.isOpened():
+while cap.isOpened() and empty_bounds:
     ret, frame = cap.read()
     if not ret:
         break
@@ -62,10 +63,41 @@ while cap.isOpened():
     if results:
         annotated_frame = results[0].plot()
         for i in range(4):
-            if bounds[i] is None:
+            if bounds[i] == None:
                 if i == 0:
-                    cv2.imshow('Pose Estimation', annotated_frame)
+                    cv2.imshow(
+                        'Press s to select p' + str(i) + ': Throttle Low position', annotated_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('s'):
+                        bounds[i] = results[0].keypoints.xyn[0][throt_move] - \
+                            results[0].keypoints.xyn[0][throt_ref]
+                elif i == 1:
+                    cv2.imshow(
+                        'Press s to select: Throttle High position', annotated_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('s'):
+                        bounds[i] = results[0].keypoints.xyn[0][throt_move] - \
+                            results[0].keypoints.xyn[0][throt_ref]
+                elif i == 2:
+                    cv2.imshow(
+                        'Press s to select: Turn Low position', annotated_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('s'):
+                        bounds[i] = results[0].keypoints.xyn[0][turn_move] - \
+                            results[0].keypoints.xyn[0][turn_ref]
 
+                elif i == 3:
+                    cv2.imshow(
+                        'Press s to select: Turn High position', annotated_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('s'):
+                        bounds[i] = results[0].keypoints.xyn[0][turn_move] - \
+                            results[0].keypoints.xyn[0][turn_ref]
+                break
+            elif i == 3:
+                empty_bounds = False
+
+print(bounds)
+throt_low = bounds[0]
+throt_high = bounds[1]
+turn_low = bounds[2]
+turn_high = bounds[3]
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -86,19 +118,34 @@ while cap.isOpened():
         if len(points) != 0:
 
             # Print difference in coordinates of turn mover and turn base
-            t_move_xy = points[turn_move].numpy()
-            t_base_xy = points[turn_ref].numpy()
+            turn_move_xy = points[turn_move].numpy()
+            turn_base_xy = points[turn_ref].numpy()
 
             # All zeros -> not detected -> false
-            found_t_move = not np.all(t_move_xy == 0)
-            found_t_base = not np.all(t_base_xy == 0)
-            if found_t_move and found_t_base:
-                raw_diff = t_move_xy - t_base_xy
-                print("raw diff: " + str(raw_diff))
+            found_turn_move = not np.all(turn_move_xy == 0)
+            found_turn_base = not np.all(turn_base_xy == 0)
+            if found_turn_move and found_turn_base:
+                turn_diff = turn_move_xy - turn_base_xy
+                # print("raw diff: " + str(turn_diff))
 
-                slider = np.dot(turn_line, raw_diff) / \
+                slider = np.dot(turn_line, turn_diff) / \
                     np.dot(turn_line, turn_line)
-                print("slider: " + str(slider))
+                print("Turn slider: " + str(slider))
+
+            # Print difference in coordinates of throt mover and throt base
+            throt_move_xy = points[throt_move].numpy()
+            throt_base_xy = points[throt_ref].numpy()
+
+            # All zeros -> not detected -> false
+            found_throt_move = not np.all(throt_move_xy == 0)
+            found_throt_base = not np.all(throt_base_xy == 0)
+            if found_throt_move and found_throt_base:
+                throt_diff = throt_move_xy - throt_base_xy
+                # print("raw diff: " + str(raw_diff))
+
+                slider = np.dot(turn_line, throt_diff) / \
+                    np.dot(turn_line, turn_line)
+                print("Throt slider: " + str(slider))
 
     if results and cv2.waitKey(1) & 0xFF == ord('q'):
         break
